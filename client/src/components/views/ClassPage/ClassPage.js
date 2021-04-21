0cfaaca541d2c5497607494a971f0148f6863cd9
Gyeong-Yeon <lky4156@naver.com>
2021-04-21 16:18:36 +0900

    feat: 🎸 0421 front add Navbar to Mainpage
    feat: 🎸 0421 front add class tab to route classpage
    
    ✅ Closes: #S04P31D102-33

--- client/src/components/views/ClassPage/ClassPage.js
+++ client/src/components/views/ClassPage/ClassPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function ClassPage() {
-    return <div>ClassPage</div>;
+    return (
+        <div>
+            <h1>ClassPage</h1>
+        </div>
+    );
 }
 
 export default ClassPage;
