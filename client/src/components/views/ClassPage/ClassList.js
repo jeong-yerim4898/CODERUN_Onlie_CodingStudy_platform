be3aeeac8b8d864bcb82691cf65d6d54c42bd5e4
DESKTOP-KVCQHCD\multicampus <op032@naver.com>
2021-04-22 17:33:07 +0900

    feat: 🎸 create class page input, language tag

--- client/src/components/views/ClassPage/ClassList.js
+++ client/src/components/views/ClassPage/ClassList.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { Space, Button, Input, Card, Row, Col } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Space, Input, Card, Row, Col } from 'antd';
 import './ClassList.css';
+import LanguageBox from './Sections/LanguageBox';
+import { languages } from './Sections/Languages';
 
 function ClassList() {
+    const [Filters, setFilters] = useState({
+        languages: [],
+    });
+    const [Skip, setSkip] = useState(0);
+    // const [Limit, setLimit] = useState(8);
     const { Meta } = Card;
     const { Search } = Input;
+
+    useEffect(() => {
+        let body = {
+            skip: Skip,
+            // limit: Limit,
+        };
+        // getProducts(body);
+    }, []);
+
     const Classes = [
         { title: 'what u code do', nickname: 'paccoding' },
         { title: 'what u code do2', nickname: 'paccoding2' },
@@ -17,27 +33,51 @@ function ClassList() {
         { title: 'what u code do4', nickname: 'paccoding3' },
         { title: 'what u code do4', nickname: 'paccoding3' },
     ];
-    const renderCards = Classes.map((product, index) => {
+    const renderCards = Classes.map((classs, index) => {
         return (
-            // <a className="Class-atag" href={'/videoclass/detail/' + product.id} key={index}>
-            <Col className="colcard" span={5}>
-                <Card
-                    hoverable
-                    cover={
-                        <img
-                            alt="example"
-                            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                        />
-                    }
-                    title={product.title}
-                    bordered={false}
-                >
-                    {product.nickname}
-                </Card>
-            </Col>
-            // </a>
+            <a href={'/watch/' + index} key={index}>
+                <Col className="colcard" span={5}>
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                            />
+                        }
+                        title={classs.title}
+                        bordered={false}
+                    >
+                        {classs.nickname}
+                    </Card>
+                </Col>
+            </a>
         );
     });
+
+    const showFilteredResults = filters => {
+        let body = {
+            skip: 0,
+            // limit: Limit,
+            filters: filters,
+        };
+
+        // getProducts(body);
+        setSkip(0);
+    };
+
+    const handleFilters = (filters, category) => {
+        const newFilters = { ...Filters };
+
+        newFilters[category] = filters;
+
+        console.log('filters', filters);
+
+        showFilteredResults(newFilters);
+        setFilters(newFilters);
+    };
+
     return (
         <div>
             <Space size={2}>
@@ -48,13 +88,12 @@ function ClassList() {
                     size="large"
                     // onSearch={onSearch}
                 />
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
+                <LanguageBox
+                    list={languages}
+                    handleFilters={filters => handleFilters(filters, 'languages')}
+                />
             </Space>
-            <Row className="board" gutter={16}>
-                {renderCards}
-            </Row>
+            <Row className="board">{renderCards}</Row>
         </div>
     );
 }
---
+++ client/src/components/views/ClassPage/Sections/Languages.js
@@ -0,0 +1,20 @@
+const languages = [
+    {
+        id: 1,
+        name: 'C/C++',
+    },
+    {
+        id: 2,
+        name: 'Java',
+    },
+    {
+        id: 3,
+        name: 'Python',
+    },
+    {
+        id: 4,
+        name: 'JavaScript',
+    },
+];
+
+export { languages };
