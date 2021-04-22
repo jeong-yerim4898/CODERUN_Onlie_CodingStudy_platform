ac87cd2dcc1de9f580738114d95a8f7c54475b9b
fksk94 <fksk94@gmail.com>
2021-04-22 12:32:08 +0900

    feat: 🎸 deploy

--- server/main/app/main.py
+++ server/main/app/main.py
@@ -27,4 +27,4 @@ app.include_router(video_router)
 app.include_router(board_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
