748115bc314cdb797dc566f82c628672da50e991
DESKTOP-KVCQHCD\multicampus <op032@naver.com>
2021-04-29 15:36:45 +0900

    feat: 🎸 create article create, list
    ✅ Closes: #S04P31D102-100

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
                  true -> 로그인한 유저만 출입이 가능한 페이지
                  false -> 로그인한 유저가 account 페이지 가려고 하면 막아준다
   */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (SpecificComponent, option) {
     const AuthenticationCheck = props => {
         let user = useSelector(state => state.user);
