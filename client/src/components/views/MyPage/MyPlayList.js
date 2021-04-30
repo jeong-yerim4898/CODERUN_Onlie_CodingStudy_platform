c7d80a8dcc87c32efaf1a6537fecba7e44a6fd87
Gyeong-Yeon <lky4156@naver.com>
2021-04-30 16:42:06 +0900

    feat: 🎸 0430 front myplaylistcreateform making
    ✅ Closes: #S04P31D102-105

--- client/src/components/views/MyPage/MyPlayList.js
+++ client/src/components/views/MyPage/MyPlayList.js
@@ -1,41 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Modal, Form } from 'react-bootstrap';
+import { readPlaylist, updatePlaylist, deletePlaylist } from '_api/Playlist.js';
+import MyPlayListCreateForm from './MyPlayListCreateForm.js';
 
 function MyPlayList() {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                플레이리스트 만들기
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>제목제목</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form.Group>
-                        <Form.Control size="lg" type="text" placeholder="Large text" />
-                        <br />
-                        <Form.Control type="text" placeholder="Normal text" />
-                        <br />
-                        <Form.Control size="sm" type="text" placeholder="Small text" />
-                    </Form.Group>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        취소
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        저장
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+        <div>
+            <MyPlayListCreateForm />
+        </div>
     );
 }
 
--- client/src/components/views/MyPage/MyPlayListCreateForm.js
+++ client/src/components/views/MyPage/MyPlayListCreateForm.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import MyPlayList from './MyPlayList.js';
+import React, { useState } from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { createPlaylist } from '_api/Playlist.js';
 
-function MyPlayListCreateForm() {
+function MyPlayListCreateForm(props) {
+    const history = useHistory();
+    const [Title, setTitle] = useState('');
+
+    const titleChangeHandler = e => {
+        setTitle(e.currentTarget.value);
+    };
+
+    const submitHandler = event => {
+        event.preventDefault();
+        console.log(Title);
+        if (!Title) {
+            return alert('제목을 입력해 주셔야 합니다.');
+        }
+        const body = {
+            title: Title,
+        };
+        createPlaylist(body)
+            .then(res => {
+                console.log(res);
+                console.log('success');
+                props.history.push('/profile');
+            })
+            .catch(err => console.log(err));
+    };
     return (
         <div>
-            <MyPlayList />
+            <InputGroup className="mb-3">
+                <FormControl
+                    onChange={titleChangeHandler}
+                    value={Title}
+                    placeholder="My Playlist"
+                />
+                <InputGroup.Append>
+                    <Button variant="outline-secondary" type="submit" onClick={submitHandler}>
+                        Button
+                    </Button>
+                </InputGroup.Append>
+            </InputGroup>
         </div>
     );
 }
