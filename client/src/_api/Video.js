f3b350ef423e01aee1e6aa313ab16a8a78cd4fd5
jeong-yerim4898 <jinju151002@gmail.com>
2021-05-03 18:21:17 +0900

    feat: 🎸 add video upload (video)
    ✅ Closes: #S04P31D102-174

--- client/src/_api/Video.js
+++ client/src/_api/Video.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 import { SERVER, ACCESS_TOKEN } from 'Config.js';
 
-export function postVideoUpload(video_info) {
+// 비디오 업로드 (내용)
+export function postVideoContentUpload(video_info) {
     return axios.post(`${SERVER}/api/video/create`, video_info, {
         headers: { token: `${ACCESS_TOKEN}` },
+    });
+}
+
+// 비디오 업로드 (비디오)
+export function postVideoUpload(video_id, file_extension, video_info) {
+    return axios.post(`${SERVER}/video/create/${video_id}/${file_extension}`, video_info, {
+        headers: { token: `${ACCESS_TOKEN}`, 'Content-Type': 'multipart/form-data' },
     });
 }
