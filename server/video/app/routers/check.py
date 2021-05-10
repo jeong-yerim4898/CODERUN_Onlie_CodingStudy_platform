b109ea420c49638bb0a6a6571a1301b05e5d37e3
fksk94 <fksk94@gmail.com>
2021-05-10 15:51:40 +0900

    feat: 🎸 progress API
    ✅ Closes: #S04P31D102-190

--- server/video/app/routers/check.py
+++ server/video/app/routers/check.py
@@ -20,3 +20,24 @@ parent_route = path.dirname(path.abspath(path.dirname(__file__)))
 
 
 # 확인 프로세스 만들기
+@router.get("/video/ready", tags=["check"], description="비디오 API 준비확인")
+def ready_for_video():
+    return {"data":"success"}
+
+
+# 프로그레스 만들기
+@router.get("/video/progress/{video_id}", tags=["check"], description="업로드 확인")
+def encoding_progress(video_id):
+    video_data = open(f"videos/{video_id}_VIDEO.m3u8", mode="r").read().split('\n')
+    if video_data[-1] == "#EXT-X-ENDLIST" or video_data[-2] == "#EXT-X-ENDLIST":
+        return {"data":100}
+    if (len(video_data) - 4)//2 <= 10:
+        return {"data": (len(video_data) - 4)//2 * 4}
+    elif (len(video_data) - 4)//2 <= 20:
+        return {"data": 40 + ((len(video_data) - 4)//2-10) * 3}
+    elif (len(video_data) - 4)//2 <= 30:
+        return {"data": 70 + ((len(video_data) - 4)//2-20) * 2}
+    elif (len(video_data) - 4)//2 <= 38:
+        return {"data": 90 + ((len(video_data) - 4)//2-30) * 1}
+    else:
+        return {"data": 99}
