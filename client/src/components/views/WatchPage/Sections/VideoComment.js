e09f37ebd747433be4a1d088b050dfe1ab66e7cd
jeong-yerim4898 <jinju151002@gmail.com>
2021-05-10 15:28:35 +0900

    feat: 🎸 add comment infinite scroll
    ✅ Closes: #S04P31D102-207

--- client/src/components/views/WatchPage/Sections/VideoComment.js
+++ client/src/components/views/WatchPage/Sections/VideoComment.js
@@ -12,18 +12,32 @@ function VideoComment(props) {
         props.removeComment(props.VideoComment.id);
     };
     const handlerUpdate = e => {
-        setcommentUpdate(e.currentTarget.value);
+        setcommentUpdate(e.currengtTarget.value);
     };
     const updateComment = () => {
         const updateBtn = document.getElementsByClassName(`${props.VideoComment.id}comment-update`);
         const content = document.getElementsByClassName(`${props.VideoComment.id}comment-content`);
+        const updateIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-update-icon`,
+        );
+        const deleteIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-delete-icon`,
+        );
         updateBtn[0].classList.remove('hidden');
         content[0].classList.add('hidden');
+        updateIcon[0].classList.add('hidden');
+        deleteIcon[0].classList.add('hidden');
     };
 
     const sendCommentUpdate = () => {
         const updateBtn = document.getElementsByClassName(`${props.VideoComment.id}comment-update`);
         const content = document.getElementsByClassName(`${props.VideoComment.id}comment-content`);
+        const updateIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-update-icon`,
+        );
+        const deleteIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-delete-icon`,
+        );
         const body = { video_comment_id: props.VideoComment.id, content: commentUpdate };
         updateVideoComment(body)
             .then(res => {
@@ -31,9 +45,26 @@ function VideoComment(props) {
                 setcomment(commentUpdate);
                 updateBtn[0].classList.add('hidden');
                 content[0].classList.remove('hidden');
+                updateIcon[0].classList.remove('hidden');
+                deleteIcon[0].classList.remove('hidden');
             })
             .catch(err => console.log(err));
         setcomment(commentUpdate);
+    };
+    const cancleCommentUpdate = () => {
+        const updateBtn = document.getElementsByClassName(`${props.VideoComment.id}comment-update`);
+
+        const updateIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-update-icon`,
+        );
+        const deleteIcon = document.getElementsByClassName(
+            `${props.VideoComment.id}comment-delete-icon`,
+        );
+        const content = document.getElementsByClassName(`${props.VideoComment.id}comment-content`);
+        updateBtn[0].classList.add('hidden');
+        content[0].classList.remove('hidden');
+        updateIcon[0].classList.remove('hidden');
+        deleteIcon[0].classList.remove('hidden');
     };
     return (
         <div>
@@ -47,13 +78,20 @@ function VideoComment(props) {
                         <h4>{props.VideoComment.user.name}</h4>
                         {props.VideoComment.user.id == user.login.user.id ? (
                             <div>
-                                <EditOutlined onClick={updateComment} />
-                                <CloseOutlined onClick={deleteComment} />
+                                <EditOutlined
+                                    className={props.VideoComment.id + 'comment-update-icon'}
+                                    onClick={updateComment}
+                                />
+                                <CloseOutlined
+                                    className={props.VideoComment.id + 'comment-delete-icon'}
+                                    onClick={deleteComment}
+                                />
                                 <div
                                     className={props.VideoComment.id + 'comment-update' + ' hidden'}
                                 >
                                     <Input onChange={handlerUpdate} />
                                     <Button onClick={sendCommentUpdate}>수정완료</Button>
+                                    <Button onClick={cancleCommentUpdate}>취소</Button>
                                 </div>
                             </div>
                         ) : (
